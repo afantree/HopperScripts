3a1f9f2d00d8beeaa1e0c1d648d5e627cef7fecd
阿凡树 <729397005@qq.com>
2017-05-16 15:06:21 +0800

    update for new hopper sample

--- Create Procedures.py
+++ Create Procedures.py
@@ -18,13 +18,7 @@ while addr < last:
     # Look for the "push ebp / mov ebp, esp" pattern
     if doc.is64Bits() and seg.readByte(addr) == 0x55 and seg.readByte(addr + 1) == 0x48 and seg.readByte(addr + 2) == 0x89 and seg.readByte(addr + 3) == 0xE5:
         seg.markAsProcedure(addr)
-	addr += 3
-
-    x = seg.readByte(addr)
-    y = seg.readByte(addr + 1)
-    z = seg.readByte(addr + 2)
-    if not doc.is64Bits() and x == 0x55 and y in (0x89, 0x8B) and z in (0xE5, 0xEC):
+    if not doc.is64Bits() and seg.readByte(addr) == 0x55 and seg.readByte(addr + 1) == 0x89 and seg.readByte(addr + 2) == 0xE5:
         seg.markAsProcedure(addr)
-	addr += 2
 
     addr = addr + 1
